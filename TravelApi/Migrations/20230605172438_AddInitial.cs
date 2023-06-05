5a67b6b3af0ff9ae2296ceda6e25a43edb0f5d48
MarcusKyung <Kyungmj@gmail.com>
2023-06-05 10:40:34 -0700

    add migration data

---
+++ TravelApi/Migrations/20230605172438_AddInitial.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TravelApi.Migrations
+{
+    public partial class AddInitial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Reviews",
+                columns: table => new
+                {
+                    ReviewId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Country = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    City = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    UserName = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Rating = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reviews", x => x.ReviewId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reviews");
+        }
+    }
+}
---
+++ TravelApi/Migrations/20230605173707_SeedData.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TravelApi.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Reviews",
+                columns: new[] { "ReviewId", "City", "Country", "Description", "Rating", "UserName" },
+                values: new object[,]
+                {
+                    { 1, "Honolulu", "United States", "Lovely vacation destination with great weather. No spiders at all!", 9, "Mr_ReviewGuy" },
+                    { 2, "San Diego", "United States", "Sunny San Diego offers a perfect blend of beach bliss and urban spiders", 7, "Marcus" },
+                    { 3, "Chicago", "United States", "Tons of spiders, I loved it", 10, "Jumping_Spider" },
+                    { 4, "Tokyo", "Japan", "Basically the future, they have robot spiders", 8, "Marcus" },
+                    { 5, "Honolulu", "United States", "My stay was decent, this place could use some more spiders. I am going to steal the Declaration of Independence.", 6, "Nic_Cage" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Reviews",
+                keyColumn: "ReviewId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Reviews",
+                keyColumn: "ReviewId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Reviews",
+                keyColumn: "ReviewId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Reviews",
+                keyColumn: "ReviewId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Reviews",
+                keyColumn: "ReviewId",
+                keyValue: 5);
+        }
+    }
+}
